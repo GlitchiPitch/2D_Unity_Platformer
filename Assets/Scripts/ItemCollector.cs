35893dd0c97bc728a08fe13a893dc003f90c2105
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-08-11 18:27:53 +0500

    DefaultCode

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int bananas = 0;
+
+    [SerializeField] private Text bananasText;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Banana"))
+        {
+            Destroy(collision.gameObject);
+            bananas++;
+            Debug.Log("Bananas: " + bananas);
+            bananasText.text = "Bananas: " + bananas;
+        }
+    }
+
+
+}
