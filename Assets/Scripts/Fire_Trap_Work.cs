a4fa81f0d4f3b57b9b07a29b220cff36ba3beb79
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-08-13 20:13:59 +0500

    start create fire trap

---
+++ Assets/Scripts/Fire_Trap_Work.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_Trap_Work : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        anim = GetComponent<Animator>(); 
+    }
+
+    private void Update()
+    {
+
+    }
+}
